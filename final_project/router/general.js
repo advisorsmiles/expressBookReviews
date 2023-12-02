6fa507cee97b72acad8335770d5ecc48aa88fca8
advisorsmiles <148494782+advisorsmiles@users.noreply.github.com>
2023-12-01 21:28:19 -0700

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,11 +4,8 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-/*express.post()is a method meant to serve the create requests to the serve. It has two parameters, first defines the end point and 
-the second is a function taking the request-handler and the response handler*/
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
   const username = req.body.username;
   const password = req.body.password;
   
@@ -23,12 +20,9 @@ public_users.post("/register", (req,res) => {
   return res.status(404).json({message: "Error not able to register."});
 });
 
-/* express.get() is a method meant to serve the retrieve requests to the server. It has two parameters;first defines the end point and 
-second is a function taking the request-handler and the response handler*/
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
   res.send(JSON.stringify(books, null, 4));
   
 });
@@ -36,7 +30,6 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
   const isbn = req.params.isbn;
   res.send(books[isbn]);
   
@@ -44,7 +37,6 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
   findAuthor = req.params.author;
   booksList = books;
   newList = {};
@@ -65,7 +57,6 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
   findTitle = req.params.title;
   booksList = books;
   newList = {};
@@ -84,7 +75,6 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
   findIsbn = req.params.isbn;
   booksList = books;
   newList = {};
