65ed1c27fbb7b76c7f9978208d03c47d1fe7f7b5
advisorsmiles <148494782+advisorsmiles@users.noreply.github.com>
2023-12-01 21:29:24 -0700

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -1,25 +1,13 @@
-/*require() is a method built in Node js and is used to incorporate external modules that are included in different files. 
-The require() statment essentially reads and executes a Java Script file before returning the export*/
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
-/*The app.use() function is used to mount the specified middleware function(s) at the path which is being specified. 
-It is mostly used to set up middleware for your application. 
-app.use(path,callback)
-
-Parameters:
-
-path: It is the path for which the middleware function is being called. It can be a string representing a path or path pattern or a regular expression pattern to match the paths.
-callback: It is a middleware function or a series/array of middleware functions.*/
-
 const app = express();
 
 app.use(express.json());
 
-//a session object with user-defined secret as a middle ware to intercept the requests and ensure that the sessionis valid before processing
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true})) 
 
 app.use("/customer/auth/*", function auth(req,res,next){
