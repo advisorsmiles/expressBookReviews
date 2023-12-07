ff69814a81e6ec5e0978e60c4637e7943ee05b77
advisorsmiles <148494782+advisorsmiles@users.noreply.github.com>
2023-12-07 14:51:21 -0700

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -22,14 +22,18 @@ public_users.post("/register", (req,res) => {
 
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/books',function (req, res) {
   res.send(JSON.stringify(books, null, 4));
-  
+  const getBooks = new Promise((resolve, reject) => {
+    resolve(res.send(JSON.stringify({books}, null, 4)));
+  });
+
+  getBooks.then(() => console.log("Promise for Task 10"));
 });
 
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+// Get book details based on ISBN Promise
+public_users.get('/books/isbn/:isbn',function (req, res) {
     const getBooksIsbn = new Promise((resolve, reject) => {
         const isbn = req.params.isbn;
         // console.log(isbn);
@@ -42,12 +46,14 @@ public_users.get('/isbn/:isbn',function (req, res) {
         });
         getBooksIsbn
             then(function(){
-                console.log("Promise");
+                console.log("Promise for task 11");
        }).
             catch(function () { 
                     console.log('ISBN not valid');
       });
-    
+      });
+// Get book details based on ISBN 
+      public_users.get('/isbn/:isbn',function (req, res) {
   booksList = books;
   newList = {};
   
@@ -64,9 +70,33 @@ public_users.get('/isbn/:isbn',function (req, res) {
 
   });
   
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  findAuthor = req.params.author;
+// Get book details based on author Promise
+public_users.get('/books/author/:author',function (req, res) {
+    const booksByAuthor = new Promise((resolve, reject) => {
+
+        let booksByAuthor = [];
+        let ISBN = Object.keys(books);
+        isbn.forEach((isbn) => {
+          if(books[isbn]["author"] === req.params.author) {
+            booksByAuthor.push({"isbn":isbn,
+                                "title":books[isbn]["title"],
+                                "reviews":books[isbn]["reviews"]});
+          resolve(res.send(JSON.stringify({booksByAuthor}, null, 4)));
+          }
+        });
+        reject(res.send("author not valid"))
+            
+        });
+    
+        booksByAuthor.then(function(){
+                console.log("Promise resolved");
+       }).catch(function () { 
+                    console.log("author not valid");
+      });
+    });
+// Get book details based on author 
+ public_users.get('author/:author',function (req, res) { 
+ findAuthor = req.params.author;
   booksList = books;
   newList = {};
   
@@ -84,7 +114,27 @@ public_users.get('/author/:author',function (req, res) {
   });
   
 
-// Get all books based on title
+// Get all books based on title Promise
+public_users.get('/books/title/:title',function (req, res) {
+const booksByTitle = new Promise((resolve, reject) => {
+    const byTitle = req.params.title;
+    // console.log(isbn);
+        if (req.params.isbn <= 10) {
+        resolve(res.send(books[title]));
+    }
+        else {
+            reject(res.send('title not valid'));
+        }
+    });
+    booksByTitle
+        then(function(){
+            console.log("Promise for task 13");
+   }).
+        catch(function () { 
+                console.log('title not valid');
+  });
+  });
+// Get all books based on title 
 public_users.get('/title/:title',function (req, res) {
   findTitle = req.params.title;
   booksList = books;
